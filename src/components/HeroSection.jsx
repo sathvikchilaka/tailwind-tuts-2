20e82b11a9776f12ec38eb0f6796914589ccbe02
sathvik <cs34@iitbbs.ac.in>
2025-01-18 03:44:41 +0530

    Add motions to HeroSection and Technologies

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,32 +1,68 @@
-import React from "react";
 import profilePic from "../assets/raviKumarProfile.webp";
 import { HERO_CONTENT } from "../constants";
+import { motion } from "motion/react";
 
 const HeroSection = () => {
+
+  const childVariant = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+  };
+
+  const containerVariant = {
+    intial: { opacity: 0, x: -100 },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.7,
+      },
+    },
+  };
+
   return (
     <div className="flex flex-wrap lg:flex-row-reverse">
       <div className="w-full lg:w-1/2">
         <div className="flex justify-center lg:p-8">
-          <img
+          <motion.img
             src={profilePic}
             alt="Sathvik Chilaka"
             className="border border-stone-900 rounded-3xl"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.2 }}
           />
         </div>
       </div>
       <div className="w-full lg:w-1/2">
-        <div className="flex flex-col items-center lg:items-start mt-10">
-          <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+        <motion.div
+          variants={containerVariant}
+          animate='animate'
+          initial="initial"
+          className="flex flex-col items-center lg:items-start mt-10"
+        >
+          <motion.h2
+            variants={childVariant}
+            className="pb-2 text-4xl tracking-tighter lg:text-8xl"
+          >
             Sathvik Chilaka
-          </h2>
-          <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-transparent text-3xl tracking-tight">
+          </motion.h2>
+          <motion.span
+            variants={childVariant}
+            className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-transparent text-3xl tracking-tight"
+          >
             Frontend Developer
-          </span>
-          <p className="my-2 max-w-lg py-6 text-xl leading-8 tracking-tighter">
+          </motion.span>
+          <motion.p
+            variants={childVariant}
+            className="my-2 max-w-lg py-6 text-xl leading-8 tracking-tighter"
+          >
             {" "}
             {HERO_CONTENT}{" "}
-          </p>
-          <a
+          </motion.p>
+          <motion.a
+            variants={childVariant}
             href="../../SathvikChilakaResume.pdf"
             rel="noopener noreferrer"
             target="_blank"
@@ -34,8 +70,8 @@ const HeroSection = () => {
             className="bg-white rounded-3xl p-4 text-sm text-stone-800 mb-10"
           >
             Download Resume
-          </a>
-        </div>
+          </motion.a>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/TechnologiesSection.jsx
+++ src/components/TechnologiesSection.jsx
@@ -1,24 +1,101 @@
-import { BiLogoPostgresql } from 'react-icons/bi';
-import { FaNodeJs, FaReact } from 'react-icons/fa';
-import { RiTailwindCssFill } from 'react-icons/ri';
-import { SiMongodb } from 'react-icons/si';
-import { TbBrandNextjs } from 'react-icons/tb';
-
+import { BiLogoPostgresql } from "react-icons/bi";
+import { FaNodeJs, FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { motion } from "motion/react";
 
 const TechnologiesSection = () => {
+  const iconVariants = (duration) => {
+    return {
+      initial: { y: -15 },
+      animate: {
+        y: [15, -15],
+        transition: {
+          duration: duration,
+          ease: "linear",
+          repeat: Infinity,
+          repeatType: "reverse",
+        },
+      },
+    };
+  };
+
   return (
     <div className="pb-24">
-        <h2 className="my-20 text-center text-4xl">Technologies</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-                <SiMongodb className='px-4 w-28 h-28 text-7xl text-green-400' />
-                <FaReact className='px-4 w-28 h-28 text-cyan-400 text-7xl' />
-                <TbBrandNextjs className='px-4 w-28 h-28 text-white text-7xl' />
-                <BiLogoPostgresql className='px-4 w-28 h-28 text-7xl text-sky-700' />
-                <FaNodeJs className='px-4 w-28 h-28 text-7xl text-green-500 ' />
-                <RiTailwindCssFill className='px-4 w-28 h-28 text-7xl text-sky-500 ' />
-        </div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        viewport={{ once: true }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <SiMongodb className="px-4 w-28 h-28 text-green-500 text-7xl" />{" "}
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <FaReact className="px-4 w-28 h-28 text-cyan-400 text-7xl" />{" "}
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <TbBrandNextjs className="px-4 w-28 h-28 text-white text-7xl" />{" "}
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <BiLogoPostgresql className="px-4 w-28 h-28 text-7xl text-sky-700" />{" "}
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <FaNodeJs className="px-4 w-28 h-28 text-7xl text-green-500 " />{" "}
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className=""
+        >
+          {" "}
+          <RiTailwindCssFill className="px-4 w-28 h-28 text-7xl text-sky-500 " />{" "}
+        </motion.div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default TechnologiesSection
+export default TechnologiesSection;
